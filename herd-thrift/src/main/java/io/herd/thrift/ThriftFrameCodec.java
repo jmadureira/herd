8bbd4471c612e95d46bfcb85019b971ace2badfc
Joao Madureira <jpmadureira@gmail.com>
2014-08-13 14:49:58 +0100

    Added a stub for a thrift frame codec.

---
+++ herd-thrift/src/main/java/io/herd/thrift/ThriftFrameCodec.java
@@ -0,0 +1,11 @@
+package io.herd.thrift;
+
+import io.netty.channel.CombinedChannelDuplexHandler;
+
+public class ThriftFrameCodec extends CombinedChannelDuplexHandler<ThriftFrameDecoder, ThriftFrameEncoder> {
+
+    public ThriftFrameCodec(int maxFrameSize) {
+        init(new ThriftFrameDecoder(maxFrameSize), new ThriftFrameEncoder());
+    }
+
+}
