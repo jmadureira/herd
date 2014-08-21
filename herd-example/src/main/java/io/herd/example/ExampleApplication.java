9372f497826aaef80fbaa5630d98d296b927626c
Joao Madureira <jpmadureira@gmail.com>
2014-08-21 14:49:13 +0100

    Added really small example to check if everything is working.

---
+++ herd-example/src/main/java/io/herd/example/ExampleApplication.java
@@ -0,0 +1,22 @@
+package io.herd.example;
+
+import io.herd.http.Http;
+import io.herd.server.Application;
+import io.herd.thrift.Thrift;
+
+public class ExampleApplication extends Application {
+
+    public static void main(String[] args) {
+        new ExampleApplication().run(args);
+    }
+
+    @Override
+    protected void initialize() {
+        registerService(
+                createService("HTTP Service", new Http(null)).listen(8080).serving(ExampleResource.class));
+        
+        registerService(
+                createService("Thrift Servive", new Thrift()).listen(9090));
+    }
+
+}
