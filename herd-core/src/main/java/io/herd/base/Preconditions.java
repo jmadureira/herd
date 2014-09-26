abdb9c61dc429342db369a898a5cc81e3b52a5b4
Joao Madureira <jpmadureira@gmail.com>
2014-09-26 12:37:22 +0100

    Added a couple more checks.

--- herd-core/src/main/java/io/herd/base/Preconditions.java
+++ herd-core/src/main/java/io/herd/base/Preconditions.java
@@ -6,6 +6,37 @@ package io.herd.base;
 public final class Preconditions {
 
     /**
+     * Ensures that the provided array has the exact specified size. Note that this method checks the length of the
+     * array and not it's contents.
+     * 
+     * @param elements An array being validated
+     * @param size The expected size of the array
+     * @param message The error message to show in case the <code>elements</code> array isn't valid.
+     * @throws NullPointerException if the array is null
+     * @throws IllegalArgumentException of the array doesn't have the expected size
+     */
+    public static final void checkArraySize(Object[] elements, int size, Object message) {
+        if (checkNotNull(elements, message).length != size) {
+            throw new IllegalArgumentException(String.valueOf(message));
+        }
+    }
+
+    /**
+     * Ensures that the provided string <code>elem</code> isn't empty i.e. is not <code>null</code> and has content.
+     * 
+     * @param elem A string being validated
+     * @param message The error message to show in case the <code>elem</code> string isn't valid.
+     * @param elem The valid provided string.
+     * @throws IllegalArgumentException if the <code>elem</code> string isn't valid.
+     */
+    public static final String checkNotEmpty(String elem, Object message) {
+        if (elem == null || elem.isEmpty()) {
+            throw new IllegalArgumentException(String.valueOf(message));
+        }
+        return elem;
+    }
+
+    /**
      * Ensure that the provided elem is not null, otherwise a {@link NullPointerException} is thrown with the provided
      * message.
      * 
@@ -17,7 +48,7 @@ public final class Preconditions {
     public static final <T> T checkNotNull(T elem, Object message) {
         if (elem == null) {
             /*
-             * too lay to check if JIT is smart enough to know that we would be doing to null checks if using
+             * too lazy to check if JIT is smart enough to know that we would be doing tull checks if using
              * String.valueOf()
              */
             throw new NullPointerException(message.toString());
