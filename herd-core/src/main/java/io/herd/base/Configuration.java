887b7ce883202fbb0e0645f3113f96a14d018a6a
Joao Madureira <jpmadureira@gmail.com>
2015-01-01 18:24:54 +0000

    Added gossiper as a default configuration property.

--- herd-core/src/main/java/io/herd/base/Configuration.java
+++ herd-core/src/main/java/io/herd/base/Configuration.java
@@ -1,5 +1,30 @@
 package io.herd.base;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.herd.gossip.DefaultGossipConfiguration;
+import io.herd.gossip.GossipConfiguration;
+
 public class Configuration {
 
+	@Valid
+	@NotNull
+	private GossipConfiguration gossiper = new DefaultGossipConfiguration();
+
+	@JsonProperty
+	public GossipConfiguration getGossiper() {
+		return gossiper;
+	}
+
+	@JsonProperty
+	public void setGossiper(GossipConfiguration gossiper) {
+		this.gossiper = gossiper;
+	}
+
+	public String toString() {
+		return gossiper.toString();
+	}
 }
